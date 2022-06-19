66738f401f0240206f7bf077fb29e18146c8ddc1
Khanh-Tuan <107778275+Khanh-Tuan@users.noreply.github.com>
2022-06-20 11:58:14 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function compute() {
+  var amount = document.getElementById("amount").value {
+    If(parseInt(amount) <1){
+      alert("Please enter a positive value");
+      document.getElementById("amount").foucs()
+      return false;
+    };
+  var rate= document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interst = amount * rate * years /100;
+  var year = new Date().getFullYear()+parseInt(years);
+  }
+  
+  function UpdateRate() {
+    var ratevalue = document.getElementById("rate").value;
+    document.getElementbyID("ratevalue").innerHTML = ratevalue;
+  }
+  
+ document.getElementbyID("result").innerHTML ="If you Deposit "+Amount+", \<br>\ at an interest rate of "+rate+"%. \<br> You will receive an amount of "+interest+", \<br>\ in the year "+year+". \<br>"
+  
+  
