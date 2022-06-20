cc7f5482b69415457cabb1251c2371ac70c836e4
Khanh-Tuan <107778275+Khanh-Tuan@users.noreply.github.com>
2022-06-20 13:02:30 +1200

    Update and rename script.js to hello.js

---
+++ hello.js
@@ -0,0 +1,21 @@
+function compute() {
+  var principal = document.getElementById("principal").value;
+    If(parseInt(principal) < 1 ) {
+      alert("Please enter a positive value");
+      document.getElementById("principal").focus();
+      return;
+      }
+
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * rate * years / 100;
+  var year = new Date().getFullYear()+parseInt(years);
+  document.getElementbyID("result").innerHTML ="If you Deposit "+principal+", \<br>\ at an interest rate of "+rate+"%. \<br> You will receive an amount of "+interest+", \<br>\ in the year "+year+". \<br>"
+  };
+  
+function UpdateRate() {
+    var rate = document.getElementById("rate").value;
+    document.getElementbyID("ratevalue").innerHTML=rate;
+  };
+
+
