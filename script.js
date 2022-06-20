eb533a38d1b3320c256464dc21ea83c23c81ec8a
Khanh-Tuan <107778275+Khanh-Tuan@users.noreply.github.com>
2022-06-20 12:17:28 +1200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function compute() {
     If(parseInt(amount) <1){
       alert("Please enter a positive value");
       document.getElementById("amount").foucs()
-      return false;
+      return;
     };
-  var rate= document.getElementById("rate").value;
+  var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
-  var interst = amount * rate * years /100;
+  var interest = amount * rate * years /100;
   var year = new Date().getFullYear()+parseInt(years);
   document.getElementbyID("result").innerHTML ="If you Deposit "+Amount+", \<br>\ at an interest rate of "+rate+"%. \<br> You will receive an amount of "+interest+", \<br>\ in the year "+year+". \<br>"
   }
